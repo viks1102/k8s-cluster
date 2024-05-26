518186af809ea79833d704fffe7fc8c3cf1b6d42
viks1102 <viks_1102@yahoo.com>
2024-05-26 21:53:19 +0100

    applying the changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/container_cluster
-resource "google_container_cluster" "primary" {
-  name                     = "primary"
+resource "google_container_cluster" "gke-cluster" {
+  name                     = "gke-cluster"
   location                 = "us-central1-a"
   remove_default_node_pool = true
   initial_node_count       = 1
