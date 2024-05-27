709115124a9512604f1297511cadbe69da68c825
viks1102 <viks_1102@yahoo.com>
2024-05-27 16:58:23 +0100

    feat:creating a np cluster

--- terraform/node-pools.tf
+++ terraform/node-pools.tf
@@ -1,46 +1,17 @@
-# # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_service_account
-# resource "google_service_account" "kubernetes" {
-#   account_id = "kubernetes-demo"
-# }
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_service_account
+resource "google_service_account" "kubernetes" {
+  account_id = "kubernetes-demo"
+}
 
-# # # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/container_node_pool
-# # resource "google_container_node_pool" "ops" {
-# #   name       = "ops"
-# #   cluster    = google_container_cluster.gke-cluster.id
-# #   node_count = 1
-
-# #   management {
-# #     auto_repair  = true
-# #     auto_upgrade = true
-# #   }
-
-# #   node_config {
-# #     preemptible  = false
-# #     machine_type = "e2-small"
-
-# #     labels = {
-# #       role = "ops"
-# #     }
-
-# #     service_account = google_service_account.kubernetes.email
-# #     oauth_scopes = [
-# #       "https://www.googleapis.com/auth/cloud-platform"
-# #     ]
-# #   }
-# # }
-
+# # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/container_node_pool
 # resource "google_container_node_pool" "ops" {
-#   name    = "ops"
-#   cluster = google_container_cluster.gke-cluster.id
+#   name       = "ops"
+#   cluster    = google_container_cluster.gke-cluster.id
+#   node_count = 1
 
 #   management {
 #     auto_repair  = true
 #     auto_upgrade = true
-#   }
-
-#   autoscaling {
-#     min_node_count = 1
-#     max_node_count = 3
 #   }
 
 #   node_config {
@@ -48,14 +19,8 @@
 #     machine_type = "e2-small"
 
 #     labels = {
-#       team = "ops"
+#       role = "ops"
 #     }
-
-#     # taint {
-#     #   key    = "instance_type"
-#     #   value  = "dev"
-#     #   effect = "NO_SCHEDULE"
-#     # }
 
 #     service_account = google_service_account.kubernetes.email
 #     oauth_scopes = [
@@ -64,37 +29,72 @@
 #   }
 # }
 
-# resource "google_container_node_pool" "dev" {
-#   name    = "dev"
-#   cluster = google_container_cluster.gke-cluster.id
+resource "google_container_node_pool" "ops" {
+  name    = "ops"
+  cluster = google_container_cluster.gke-cluster.id
 
-#   management {
-#     auto_repair  = true
-#     auto_upgrade = true
-#   }
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
 
-#   autoscaling {
-#     min_node_count = 0
-#     max_node_count = 3
-#   }
+  autoscaling {
+    min_node_count = 1
+    max_node_count = 3
+  }
 
-#   node_config {
-#     preemptible  = false
-#     machine_type = "e2-small"
+  node_config {
+    preemptible  = false
+    machine_type = "e2-small"
 
-#     labels = {
-#       team = "devops"
-#     }
+    labels = {
+      team = "ops"
+    }
 
-#     # taint {
-#     #   key    = "instance_type"
-#     #   value  = "dev"
-#     #   effect = "NO_SCHEDULE"
-#     # }
+    # taint {
+    #   key    = "instance_type"
+    #   value  = "dev"
+    #   effect = "NO_SCHEDULE"
+    # }
 
-#     service_account = google_service_account.kubernetes.email
-#     oauth_scopes = [
-#       "https://www.googleapis.com/auth/cloud-platform"
-#     ]
-#   }
-# }
+    service_account = google_service_account.kubernetes.email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
+
+resource "google_container_node_pool" "dev" {
+  name    = "dev"
+  cluster = google_container_cluster.gke-cluster.id
+
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+
+  autoscaling {
+    min_node_count = 0
+    max_node_count = 3
+  }
+
+  node_config {
+    preemptible  = false
+    machine_type = "e2-small"
+
+    labels = {
+      team = "devops"
+    }
+
+    # taint {
+    #   key    = "instance_type"
+    #   value  = "dev"
+    #   effect = "NO_SCHEDULE"
+    # }
+
+    service_account = google_service_account.kubernetes.email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
