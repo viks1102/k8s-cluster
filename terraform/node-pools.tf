f9b42f241040ffb1a206eb64fee1643b4e717878
viks1102 <viks_1102@yahoo.com>
2024-05-27 16:08:44 +0100

    node-pool changed

--- terraform/node-pools.tf
+++ terraform/node-pools.tf
@@ -3,15 +3,44 @@ resource "google_service_account" "kubernetes" {
   account_id = "kubernetes-demo"
 }
 
-# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/container_node_pool
+# # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/container_node_pool
+# resource "google_container_node_pool" "ops" {
+#   name       = "ops"
+#   cluster    = google_container_cluster.gke-cluster.id
+#   node_count = 1
+
+#   management {
+#     auto_repair  = true
+#     auto_upgrade = true
+#   }
+
+#   node_config {
+#     preemptible  = false
+#     machine_type = "e2-small"
+
+#     labels = {
+#       role = "ops"
+#     }
+
+#     service_account = google_service_account.kubernetes.email
+#     oauth_scopes = [
+#       "https://www.googleapis.com/auth/cloud-platform"
+#     ]
+#   }
+# }
+
 resource "google_container_node_pool" "ops" {
-  name       = "ops"
-  cluster    = google_container_cluster.gke-cluster.id
-  node_count = 1
+  name    = "ops"
+  cluster = google_container_cluster.gke-cluster.id
 
   management {
     auto_repair  = true
     auto_upgrade = true
+  }
+
+  autoscaling {
+    min_node_count = 0
+    max_node_count = 3
   }
 
   node_config {
@@ -19,8 +48,14 @@ resource "google_container_node_pool" "ops" {
     machine_type = "e2-small"
 
     labels = {
-      role = "ops"
+      team = "ops"
     }
+
+    # taint {
+    #   key    = "instance_type"
+    #   value  = "dev"
+    #   effect = "NO_SCHEDULE"
+    # }
 
     service_account = google_service_account.kubernetes.email
     oauth_scopes = [
