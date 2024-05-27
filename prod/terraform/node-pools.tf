07bb37b9d750256c2350de5d539811d6a01b0f30
viks1102 <viks_1102@yahoo.com>
2024-05-27 17:19:07 +0100

    creating 2nd cluster

---
+++ prod/terraform/node-pools.tf
@@ -0,0 +1,39 @@
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_service_account
+resource "google_service_account" "kubernetes" {
+  account_id = "kubernetes-demo"
+}
+
+resource "google_container_node_pool" "prod" {
+  name    = "prod"
+  cluster = google_container_cluster.gke-cluster-prod.id
+
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+
+  autoscaling {
+    min_node_count = 1
+    max_node_count = 3
+  }
+
+  node_config {
+    preemptible  = false
+    machine_type = "e2-small"
+
+    labels = {
+      team = "prod"
+    }
+
+    # taint {
+    #   key    = "instance_type"
+    #   value  = "dev"
+    #   effect = "NO_SCHEDULE"
+    # }
+
+    service_account = google_service_account.kubernetes.email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
