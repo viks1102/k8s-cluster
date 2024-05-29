b5d078d01afc6f3c8686c43a01ff14843f0c1b9e
viks1102 <viks_1102@yahoo.com>
2024-05-29 18:41:47 +0100

    wip

--- gke-module/variables.auto.tfvars
+++ gke-module/variables.auto.tfvars
@@ -1,11 +1,11 @@
-# gcp_project_id      = "dtonic-demo-k8s"
-# region              = "us-central1"
-# cluster_name        = "cluster-1"
-# zones               = ["us-central1-a", "us-central1-b", "us-central1-f"]
-# regional            = false
-# vpc_network         = "k8s-vpc"
-# vpc_subnet          = "private"
-# gke_node_pool       = "ops"
-# gke_service_account = "terraform-k8s-sa@dtonic-demo-k8s.iam.gserviceaccount.com"
+gcp_project_id      = "dtonic-demo-k8s"
+region              = "us-central1"
+cluster_name        = "cluster-1"
+zones               = ["us-central1-a", "us-central1-b", "us-central1-f"]
+regional            = false
+vpc_network         = "k8s-vpc"
+vpc_subnet          = "private"
+gke_node_pool       = "ops"
+gke_service_account = "terraform-k8s-sa@dtonic-demo-k8s.iam.gserviceaccount.com"
 
 
--- gke-module/variables.tf
+++ gke-module/variables.tf
@@ -1,51 +1,51 @@
-# variable "gcp_project_id" {
+variable "gcp_project_id" {
+    type = string
+    description = "Project ID"
+}
+variable "region" {
+    type = string
+    description = "this is for cluster region"
+}
+
+variable "cluster_name" {
+    type = string
+    description = "Cluster name"
+}
+variable "zones" {
+    type = list(string)
+    description = "List of Zones"
+}
+
+variable "regional" {
+    type = bool
+    description = "Regional or Not"
+}
+
+variable "vpc_network" {
+    type = string
+
+    description = "VPC network name"
+}
+variable "vpc_subnet" {
+    type = string
+    description = "VPC Subnet"
+}
+variable "gke_node_pool" {
+    type = string
+    description = "gke-node-pool name"
+}
+
+variable "gke_service_account" {
+    type = string
+    description = "gke service account name"
+}
+
+# variable "ip_range_pods" {
 #     type = string
-#     description = "Project ID"
-# }
-# variable "region" {
-#     type = string
-#     description = "this is for cluster region"
+#     description = "ip pod range"
 # }
 
-# variable "cluster_name" {
+# variable "ip_range_services" {
 #     type = string
-#     description = "Cluster name"
+#     description = "ip service range"
 # }
-# variable "zones" {
-#     type = list(string)
-#     description = "List of Zones"
-# }
-
-# variable "regional" {
-#     type = bool
-#     description = "Regional or Not"
-# }
-
-# variable "vpc_network" {
-#     type = string
-
-#     description = "VPC network name"
-# }
-# variable "vpc_subnet" {
-#     type = string
-#     description = "VPC Subnet"
-# }
-# variable "gke_node_pool" {
-#     type = string
-#     description = "gke-node-pool name"
-# }
-
-# variable "gke_service_account" {
-#     type = string
-#     description = "gke service account name"
-# }
-
-# # variable "ip_range_pods" {
-# #     type = string
-# #     description = "ip pod range"
-# # }
-
-# # variable "ip_range_services" {
-# #     type = string
-# #     description = "ip service range"
-# # }
